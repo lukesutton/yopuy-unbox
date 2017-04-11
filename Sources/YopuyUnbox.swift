74aa60560742558c9b3c6dacc072aca543f3fc9e
Luke Matthew Sutton <me@lukematthewsutton.com>
2017-04-11 10:34:58 +0930

    Make everything public

--- Sources/YopuyUnbox.swift
+++ Sources/YopuyUnbox.swift
@@ -2,17 +2,17 @@ import Yopuy
 import Foundation
 import Unbox
 
-protocol UnboxableResource: Resource, Unboxable {
+public protocol UnboxableResource: Resource, Unboxable {
 
 }
 
-extension UnboxableResource where Singular == Self, Collection == [Self] {
-  static func parse(singular data: Data) throws -> Singular {
+public extension UnboxableResource where Singular == Self, Collection == [Self] {
+  public static func parse(singular data: Data) throws -> Singular {
     let result: Self = try unbox(data: data)
     return result
   }
 
-  static func parse(collection data: Data) throws -> Collection {
+  public static func parse(collection data: Data) throws -> Collection {
     let result: [Self] = try unbox(data: data)
     return result
   }
